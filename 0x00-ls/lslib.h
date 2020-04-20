47b67838fb9b6b1afadb7c103719fdee25dad499
marianaplazas <marianaplazas@live.com>
2020-04-20 05:10:17 +0000

    Revert "[fix] lines"
    This reverts commit 038cb01b241ec6882b82d03ef085f7c3c70ebeb8.

--- 0x00-ls/lslib.h
+++ 0x00-ls/lslib.h
@@ -12,5 +12,8 @@
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
+unsigned int error_handler(char *directory);
+unsigned int hls(int argc, char *argv[]);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 #endif /*LSLIB_H*/
